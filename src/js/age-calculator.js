9a84a2eadcc9083457a9d496736cf870bd9eea5c
Gareth Grindeland <gt.grindeland@gmail.com>
2023-04-14 09:12:19 -0700

    add logic for .mercuryYears property to pass 2nd test.

--- src/js/age-calculator.js
+++ src/js/age-calculator.js
@@ -1,10 +1,10 @@
-//This template is where Buisiness Logic will go
-//Update the name
-
-// export default function Triangle(side1, side2, side3) {
+//Business Logic
 
 export class Age  {
   constructor(years) {
     this.earthYears = years;
+    this.mercuryYears = Math.floor(years/.24);
+    //round down to nearest whole year because we want age
+    console.log(this.mercuryYears);
   }
 }
