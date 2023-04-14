6859a056605f4d057cfd2cb59742b48c117c5c34
Gareth Grindeland <gt.grindeland@gmail.com>
2023-04-14 15:09:27 -0700

    WIP: UI logic to handle the form submission.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
-//This template is the 'entry point' and where UI Logic will go
+//UI Logic will go
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import './css/styles.css';
-// import { Age } from './js/age-calculator.js';
+import './css/styles.css';
+import { Age } from './js/age-calculator.js';
 
-// function handleSubmitForm(event) {
+function ageCalculator(years)  {
+  const newAge = new Age(years);
+  //WIP
+}
 
-// }
+function handleSubmitForm(event) {
+  event.preventDefault();
+  // document.getElementById("response").innerText = null;
+  const resultsDiv = document.getElementById("results");
+  resultsDiv.setAttribute("class") = "hidden";
+
+  const userAge = document.getElementById("user-age").value;
+  const mercuryAge = document.getElementById("mercury-output").innerText;
+  const venusAge = document.getElementById("venus-output").innerText;
+  const marsAge = document.getElementById("mars-output").innerText;
+  const jupiterAge = document.getElementById("jupiter-output").innerText;
+  ageCalculator(userAge);
+  resultsDiv.removeAttribute("class");
+}
 
 
-// window.addEventListener("load", function() {
-//   document.querySelector("#triangle-checker-form").addEventListener("submit", handleTriangleForm);
-// });
+window.addEventListener("load", function() {
+  document.querySelector("#age-calculator-form").addEventListener("submit", handleSubmitForm);
+});
