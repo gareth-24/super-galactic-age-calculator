a42c5e5ea02d1fe3f63b1630f7eef718445bdcfb
Gareth Grindeland <gt.grindeland@gmail.com>
2023-04-14 11:48:34 -0700

    Separate out tests 6-11 under a different describe to increase clarity.

--- __tests__/age-calculator.test.js
+++ __tests__/age-calculator.test.js
@@ -26,33 +26,36 @@ describe('Age', () => {
   test('should correctly return their age in Jupiter years and store it in a new property', () => {
     expect(age.jupiterYears).toEqual(2);
   });
+});
+
+describe('getYearsPassed', () => {
+  let age;  //define variable for the person's age for each test
+  const pastBirthdayAge = 18; //define a variable for the person's 18th bday
+  beforeEach(() => {
+    age = new Age(26);  
+  });
 
   test('should correctly return the number of "Earth years" passsed since a past birthday', () => {
-    const pastBirthdayAge = 18; //define a variable for the person's 18th bday
     const yearsSinceBday = age.getYearsPassed(pastBirthdayAge, 'earth');
     expect(yearsSinceBday).toEqual(8);
   });
 
   test('should correctly return the number of "Mercury years" passsed since a past birthday', () => {
-    const pastBirthdayAge = 18;
     const mercuryYearsSinceBday = age.getYearsPassed(pastBirthdayAge, 'mercury'); //specify mercury as the type of years we want to check
     expect(mercuryYearsSinceBday).toEqual(33.33);
   });
 
   test('should correctly return the number of "Venus years" passsed since a past birthday', () => {
-    const pastBirthdayAge = 18;
     const venusYearsSinceBday = age.getYearsPassed(pastBirthdayAge, 'venus'); //specify venus
     expect(venusYearsSinceBday).toEqual(12.9);
   });
 
   test('should correctly return the number of "Mars years" passsed since a past birthday', () => {
-    const pastBirthdayAge = 18;
     const marsYearsSinceBday = age.getYearsPassed(pastBirthdayAge, 'mars'); //specify mars
     expect(marsYearsSinceBday).toEqual(4.26);
   });
 
   test('should correctly return the number of "Jupiter years" passsed since a past birthday', () => {
-    const pastBirthdayAge = 18;
     const jupiterYearsSinceBday = age.getYearsPassed(pastBirthdayAge, 'jupiter'); //specify jupiter
     expect(jupiterYearsSinceBday).toEqual(.67);
   });
@@ -61,5 +64,4 @@ describe('Age', () => {
     //checks that this method handles errors for a planet that is not defined in our logic
     expect(age.getYearsPassed(18, 'nebula')).toEqual(false);
   });
-
-})
+});
