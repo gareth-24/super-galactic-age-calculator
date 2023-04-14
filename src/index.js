7af858cdc19d0a60ecc1e15295b70c960c9b83df
Gareth Grindeland <gt.grindeland@gmail.com>
2023-04-14 08:26:10 -0700

    add initial index html and js files.

---
+++ src/index.js
@@ -0,0 +1,14 @@
+//This template is the 'entry point' and where UI Logic will go
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import './css/styles.css';
+import AgeCalculator from './js/age-calculator.js';
+
+// function handleSubmitForm(event) {
+
+// }
+
+
+// window.addEventListener("load", function() {
+//   document.querySelector("#triangle-checker-form").addEventListener("submit", handleTriangleForm);
+// });
