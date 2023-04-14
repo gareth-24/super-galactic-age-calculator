56f69cb7a373ca16fdecbf685725e3f19cd657e3
Gareth Grindeland <gt.grindeland@gmail.com>
2023-04-14 15:48:35 -0700

    Add submit form event listener to update the webpage after the user fills out the form.

--- src/index.js
+++ src/index.js
@@ -1,27 +1,37 @@
-//UI Logic will go
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import { Age } from './js/age-calculator.js';
 
+//UI Logic
+
 function ageCalculator(years)  {
   const newAge = new Age(years);
-  //WIP
+  const mercuryAge = newAge.mercuryYears;
+  const venusAge = newAge.venusYears;
+  const marsAge = newAge.marsYears;
+  const jupiterAge = newAge.jupiterYears;
+  // console.log(mercuryAge);
+
+  document.getElementById("mercury-output").innerText = mercuryAge;
+  document.getElementById("venus-output").innerText = venusAge;
+  document.getElementById("mars-output").innerText = marsAge;
+  document.getElementById("jupiter-output").innerText = jupiterAge;
 }
 
 function handleSubmitForm(event) {
   event.preventDefault();
   // document.getElementById("response").innerText = null;
-  const resultsDiv = document.getElementById("results");
-  resultsDiv.setAttribute("class") = "hidden";
+  // const resultsDiv = document.getElementById("results");
+  // resultsDiv.setAttribute("class") = "hidden";
 
   const userAge = document.getElementById("user-age").value;
-  const mercuryAge = document.getElementById("mercury-output").innerText;
-  const venusAge = document.getElementById("venus-output").innerText;
-  const marsAge = document.getElementById("mars-output").innerText;
-  const jupiterAge = document.getElementById("jupiter-output").innerText;
+  // const mercuryAge = document.getElementById("mercury-output").innerText;
+  // const venusAge = document.getElementById("venus-output").innerText;
+  // const marsAge = document.getElementById("mars-output").innerText;
+  // const jupiterAge = document.getElementById("jupiter-output").innerText;
   ageCalculator(userAge);
-  resultsDiv.removeAttribute("class");
+  // resultsDiv.removeAttribute("class");
 }
 
 
